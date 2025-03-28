1bd8058ae835764b61668b9529741e564f5b8fe4
Mustafa0Ebrahim <s25399328@gmail.com>
2025-03-28 03:42:30 +0200

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,3 @@
+name = "mustafa"
+age = 17
+print(f"My name is {name} and I am {age} years old.")
---
+++ parity.py
@@ -0,0 +1,8 @@
+num = input("Enter a number: ")
+num = float(num)
+if num == 0:
+    print("Zero")
+elif num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
